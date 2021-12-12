7c140c6471c3600664f15ad3432d33bd49f1e7fc
Younes Arroubi <yarroubi@e1r11p1.1337.ma>
2021-12-12 14:12:19 +0100

    nginx container still in process

---
+++ srcs/requirements/nginx/tools/default_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update
+
+# ======================= installing Nginx ======================= #
+# ================================================================ #
+
+apt-get -y install nginx
+echo -e "\n\e[32mnginx\t\t\t\t\tinstalled\e[0m\n"
+
+# ============= installing Openssl to setup https ================ #
+# ================================================================ #
+
+apt-get -y install openssl
+echo -e "\n\e[32mopenssl\t\t\t\t\tinstalled\e[0m\n"
+
+# ================ installing some basic commands ================ #
+# ================================================================ #
+apt-get -y install vim
+echo -e "\n\e[32mvim\t\t\t\t\tinstalled\e[0m\n"
--- srcs/requirements/nginx/tools/post_setup.sh
+++ srcs/requirements/nginx/tools/post_setup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-cp -p /tools/nginx-selfsigned.{key, crt} /etc/ssl/
+
+cp -p /tools/nginx-selfsigned.key /etc/ssl/private/
+cp -p /tools/nginx-selfsigned.crt /etc/ssl/certs/
 echo -e "\n\e[32mSetting HTTPS\t\t\t\t\tdone\e[0m\n"
 
-cp /conf/* /etc/nginx/conf.d
+
+cp /conf/nginx.conf /etc/nginx/
+cp /conf/default.conf /etc/nginx/conf.d/
 echo -e "\n\e[32mSetting Nginx Configuration\t\t\t\t\tdone\e[0m\n"
+
+rm -rf /etc/nginx/sites-enabled/*
+rm -rf /etc/nginx/sites-available/*
+
+rm -rf /tools /conf
+
+service nginx start
+
+bash

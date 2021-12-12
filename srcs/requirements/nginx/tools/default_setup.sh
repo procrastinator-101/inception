08209bf45cb5648e12aa0d0bbaef4d4ab46c35a7
Younes Arroubi <yarroubi@e1r11p1.1337.ma>
2021-12-12 20:07:23 +0100

    mariadb in process

--- srcs/requirements/nginx/tools/default_setup.sh
+++ srcs/requirements/nginx/tools/default_setup.sh
@@ -13,8 +13,3 @@ echo -e "\n\e[32mnginx\t\t\t\t\tinstalled\e[0m\n"
 
 apt-get -y install openssl
 echo -e "\n\e[32mopenssl\t\t\t\t\tinstalled\e[0m\n"
-
-# ================ installing some basic commands ================ #
-# ================================================================ #
-apt-get -y install vim
-echo -e "\n\e[32mvim\t\t\t\t\tinstalled\e[0m\n"

37812d31f6d4b2bd43ec66b2fdd757b9808965a7
Younes Arroubi <yarroubi@e1r4p7.1337.ma>
2022-03-13 15:58:47 +0100

    env variables added in wp-config

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', getenv('WORDPRESS_DB') );
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress_owner' );
+define( 'DB_USER', getenv('WORDPRESS_ADMIN') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'OwnerPassword' );
+define( 'DB_PASSWORD', getenv('WORDPRESS_ADMIN_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', getenv('MYSQL_HOST_NAME') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
